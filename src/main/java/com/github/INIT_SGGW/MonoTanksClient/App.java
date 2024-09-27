05b4221824d2d532440cf3a0b6f4ba7046c8d70e
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2024-09-27 21:10:37 +0200

    Add help cli arg

--- src/main/java/com/github/INIT_SGGW/MonoTanksClient/App.java
+++ src/main/java/com/github/INIT_SGGW/MonoTanksClient/App.java
@@ -4,21 +4,17 @@ import com.github.INIT_SGGW.MonoTanksClient.utils.Args;
 import com.github.INIT_SGGW.MonoTanksClient.websocket.CustomWebSocketClient;
 import org.java_websocket.client.WebSocketClient;
 import picocli.CommandLine;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class App {
 
-    public static URI constructUrl(String host, int port, String code, String nickname, boolean debugQuickJoin) throws URISyntaxException {
+    public static URI constructUrl(String host, int port, String code, String nickname)
+            throws URISyntaxException {
         StringBuilder url = new StringBuilder(String.format("ws://%s:%d/?nickname=%s", host, port, nickname));
 
         url.append("&typeOfPacketType=string");
         url.append("&playerType=hackatonBot");
-
-        if (debugQuickJoin) {
-            url.append("&quickJoin=true");
-        }
 
         if (code != null && !code.isEmpty()) {
             url.append("&joinCode=");
@@ -29,19 +25,34 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println("[System] 🚀 Starting client...");
-
         Args arguments = new Args();
         CommandLine commandLine = new CommandLine(arguments);
         commandLine.parseArgs(args);
 
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            return;
+        }
+
+        System.out.println("[System] 🚀 Starting client...");
+
         try {
-            URI uri = constructUrl(arguments.getHost(), arguments.getPort(), arguments.getCode(), arguments.getNickname(), arguments.isDebugQuickJoin());
+            URI uri = constructUrl(
+                    arguments.getHost(),
+                    arguments.getPort(),
+                    arguments.getCode(),
+                    arguments.getNickname());
 
             System.out.println("[System] 📞 Connecting to the server: " + uri);
-            WebSocketClient client = new CustomWebSocketClient(uri);
-            client.run();
+            try {
+                WebSocketClient client = new CustomWebSocketClient(uri);
+                client.run();
+            } catch (Exception e) {
+                System.err.println("[System] 🚨 An error occurred: " + e.getMessage());
+            }
 
+        } catch (URISyntaxException e) {
+            System.err.println("[System] 🚨 Invalid URI: " + e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/github/INIT_SGGW/MonoTanksClient/utils/Args.java
+++ src/main/java/com/github/INIT_SGGW/MonoTanksClient/utils/Args.java
@@ -2,25 +2,29 @@ package com.github.INIT_SGGW.MonoTanksClient.utils;
 
 import lombok.Getter;
 import lombok.Setter;
+import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 @Getter
 @Setter
+@Command(name = "Args", mixinStandardHelpOptions = true, description = "\nJava client written for Hackathon 2024 H2 organized by KN inIT. The client is used to communicate with the server using the WebSocket protocol. Your task is to implement bot logic. Each time the game state updates on the server, it is sent to you and you have to respond with your move. The game is played on a 2D grid. The player with the most points at the end of the game wins. Let the best bot win!\n")
 public class Args {
 
-    @Option(names = { "-n", "--nickname" }, required = true, description = "Nickname")
+    @Option(names = { "-n", "--nickname" }, required = true, description = "The nickname to use for the client.")
     private String nickname;
 
-    @Option(names = { "-h", "--host" }, defaultValue = "localhost", description = "Host address")
+    @Option(names = { "-h",
+            "--host" }, defaultValue = "localhost", description = "The host address of the server. Default is 'localhost'.")
     private String host;
 
-    @Option(names = { "-p", "--port" }, defaultValue = "5000", description = "Port number")
+    @Option(names = { "-p",
+            "--port" }, defaultValue = "5000", description = "The port number to connect to. Default is 5000.")
     private int port;
 
-    @Option(names = { "-c", "--code" }, description = "Join code needed to join a server with a password")
+    @Option(names = { "-c",
+            "--code" }, description = "The join code required to connect to a password-protected server.")
     private String code;
 
-    @Option(names = {
-            "--debug-quick-join" }, defaultValue = "false", description = "Enable debug quick join (as soon as you connect to the server, game will start)")
-    private boolean debugQuickJoin;
+    @Option(names = { "--help" }, usageHelp = true, description = "Display this help message and exit.")
+    private boolean help;
 }
