ac4f85aff6e1c68c834486f8ca247cc4066691e2
Szymon Rosowski <rosowskiszymon@gmail.com>
2024-10-27 08:42:21 +0100

    added ZoneWrapper functionality

--- src/main/java/com/github/INIT_SGGW/MonoTanksBot/Bot/wrappers/ZoneWrapper.java
+++ src/main/java/com/github/INIT_SGGW/MonoTanksBot/Bot/wrappers/ZoneWrapper.java
@@ -19,4 +19,31 @@ public class ZoneWrapper {
                 && y >= zone.getY()
                 && y < zone.getY() + zone.getHeight();
     }
+
+    public Status getStatus() {
+        return switch (zone.getStatus()) {
+            case Zone.ZoneStatus.Neutral neutral -> Status.NEUTRAL;
+            case Zone.ZoneStatus.BeingCaptured beingCaptured -> Status.BEING_CAPTURED;
+            case Zone.ZoneStatus.Captured captured -> Status.CAPTURED;
+            case Zone.ZoneStatus.BeingContested beingContested -> Status.BEING_CONTESTED;
+            case null, default -> throw new IllegalStateException("Unknown zone status");
+        };
+    }
+
+    public OwnedBy owner() {
+        return switch (zone.getStatus()) {
+            case Zone.ZoneStatus.Neutral neutral -> OwnedBy.NOONE;
+            case Zone.ZoneStatus.BeingCaptured beingCaptured -> OwnedBy.ENEMY;
+            case Zone.ZoneStatus.Captured captured -> OwnedBy.US;
+            case Zone.ZoneStatus.BeingContested beingContested -> OwnedBy.NOONE; // TODO: check if it should be noone
+            case null, default -> throw new IllegalStateException("Unknown zone status");
+        };
+    }
+
+    enum Status {
+        NEUTRAL, BEING_CAPTURED, CAPTURED, BEING_CONTESTED
+    }
+    enum OwnedBy {
+        NOONE, US, ENEMY
+    }
 }
